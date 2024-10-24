fb31dd2be37a9f10f9b250ae9faf61d818ba3cab
Mileto Di Marco <mileto.di.marco@howest.be>
2024-10-24 10:31:59 +0200

    Add project files.

---
+++ Wba.EfBasics.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wba.EfBasics.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
